6a60c07602ff06344535c87e488ae181fd405dee
nzeemin <nzeemin@d94ecfc0-367c-4f9a-5c3d-69f45c0ea587>
2011-04-25 14:42:42 +0000

    [*] gameboy/SeaDragon: landscape scrolling; menu mode / game mode.

--- trunk/gameboy/SeaDragon/!compile.bat
+++ trunk/gameboy/SeaDragon/!compile.bat
@@ -1,5 +1,16 @@
 @echo off
+
 set path=%path%;C:\bin\RGBDS
+
 rgbasm -omain.obj main.asm
+IF ERRORLEVEL 1 GOTO END
+
 xlink -mmap seadragon.lnk
+IF ERRORLEVEL 1 GOTO END
+
 rgbfix -v seadragon
+IF ERRORLEVEL 1 GOTO END
+
+echo SUCCESSED
+
+:END
